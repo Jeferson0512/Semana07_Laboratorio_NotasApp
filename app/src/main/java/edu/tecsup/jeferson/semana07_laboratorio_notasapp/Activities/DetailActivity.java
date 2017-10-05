2d133059de5870775548faae10741de83a6bd735
Jeferson0512 <jeferson.bujaico@tecsup.edu.pe>
2017-10-05 12:57:50 -0500

    Version2

--- app/src/main/java/edu/tecsup/jeferson/semana07_laboratorio_notasapp/Activities/DetailActivity.java
+++ app/src/main/java/edu/tecsup/jeferson/semana07_laboratorio_notasapp/Activities/DetailActivity.java
@@ -2,14 +2,57 @@ package edu.tecsup.jeferson.semana07_laboratorio_notasapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import edu.tecsup.jeferson.semana07_laboratorio_notasapp.R;
+import edu.tecsup.jeferson.semana07_laboratorio_notasapp.repositorires.NoteRepository;
+import edu.tecsup.jeferson.semana07_laboratorio_notasapp.models.Note;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = DetailActivity.class.getSimpleName();
+
+    private Long id;
+
+    private TextView titleText;
+    private TextView contentText;
+    private CheckBox favoriteStar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        // Enable back button with onSupportNavigateUp method
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        titleText = (TextView)findViewById(R.id.title_text);
+        contentText = (TextView)findViewById(R.id.content_text);
+        favoriteStar = (CheckBox)findViewById(R.id.favorite_star);
+
+        id = getIntent().getExtras().getLong("ID");
+        Log.e(TAG, "ID: " + id);
+
+        // Get Note by ID from Repository
+        Note note = NoteRepository.get(id);
+
+        titleText.setText(note.getTitle());
+        contentText.setText(note.getContent());
+        if(note.getFavorite()){
+            favoriteStar.setVisibility(View.VISIBLE);   // Hidden view
+        }else{
+            favoriteStar.setVisibility(View.GONE);  // Show view
+        }
+
     }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+
 }
--- app/src/main/java/edu/tecsup/jeferson/semana07_laboratorio_notasapp/repositorires/NoteRepository.java
+++ app/src/main/java/edu/tecsup/jeferson/semana07_laboratorio_notasapp/repositorires/NoteRepository.java
@@ -1,13 +1,10 @@
 package edu.tecsup.jeferson.semana07_laboratorio_notasapp.repositorires;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import edu.tecsup.jeferson.semana07_laboratorio_notasapp.models.Note;
-
-/**
- * Created by Alumno on 10/2/2017.
- */
 
 public class NoteRepository {
 
@@ -21,10 +18,30 @@ public class NoteRepository {
 
     private static long sequence = 1;
 
-    public static Note add (Note note){
+    public static Note add(Note note){
         note.setId(sequence++);
         notes.add(0, note);
         return note;
     }
-    
+
+    public static Note get(Long id){
+        for (Note note : notes) {
+            if(note.getId().equals(id))
+                return note;
+        }
+        return null;
+    }
+
+    public static Note remove(Long id){
+        for(Iterator<Note> i = notes.iterator(); i.hasNext();) {
+            Note note = i.next();
+            if(note.getId().equals(id)) {
+                i.remove();
+                return note;
+            }
+        }
+        return null;
+    }
+
+
 }
